2fbc84114498a1a5cee679bc96a28048b7fe6ac3
johao <johaomarcos1995@gmail.com>
2022-06-02 20:12:59 +0200

    Avance 2

--- apps/contacto/models.py
+++ apps/contacto/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Contacto(models.Model):
     name=models.CharField(max_length=40,blank=False,verbose_name='Nombre')
     email=models.EmailField(max_length=40,blank=False,verbose_name='Email')
-    message=models.TextField(blank=False,verbose_name='Mensaje')
+    message=models.CharField(blank=False,verbose_name='Mensaje')
 
     class Meta:
         verbose_name_plural='Contacto'
